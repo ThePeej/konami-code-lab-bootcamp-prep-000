03d61e5f1d2a2b08da9b2d3385223c7354e51c68
PJ Frias <peejfrias@gmail.com>
2017-11-05 00:18:24 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,8 +3,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   const body = document.querySelector('body')
-
+  var cheats = []
   body.addEventListener('keydown', function(e) {
     console.log(e.which)
+    cheats.push(parseInt(e.which))
+    console.log(cheats)
   })
 }
