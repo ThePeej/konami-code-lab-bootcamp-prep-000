a2390038a412d02268d1a722739fe8804b2da837
PJ Frias <peejfrias@gmail.com>
2017-11-05 00:26:04 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,7 +6,7 @@ function init() {
   const body = document.querySelector('body')
   var cheats = []
   body.addEventListener('keydown', function(e) {
-    console.log(e.which)
+    console.log(e.detail || e.which)
     cheats.push(parseInt(e.detail || e.which))
     console.log(cheats)
   })
