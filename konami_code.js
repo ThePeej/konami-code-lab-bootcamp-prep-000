67c0202204b0ff437d2f003ece5e4e8b6132579b
PJ Frias <peejfrias@gmail.com>
2017-11-05 00:56:30 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-const index = 0
 
 function init() {
   // Write your JavaScript code inside the init() function
   const body = document.querySelector('body')
+  const index = 0
   var cheats = []
   body.addEventListener('keydown', function(e) {
     console.log(e.detail || e.which)
